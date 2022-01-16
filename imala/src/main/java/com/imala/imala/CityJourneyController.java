519bba4c43eafbee8b06672b652452b32a784497
Abel-Mekonen <abelmekonen559@gmail.com>
2022-01-17 02:53:29 +0300

    city journey tariff search added

--- imala/src/main/java/com/imala/imala/CityJourneyController.java
+++ imala/src/main/java/com/imala/imala/CityJourneyController.java
@@ -1,9 +1,12 @@
 package com.imala.imala;
 
-
+// import java.lang.System.Logger;
 import java.util.List;
 
+// import org.hibernate.annotations.common.util.impl.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Component;
+// import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 // import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,136 +15,117 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class CityJourneyController {
-    
+
     @Autowired
     private CityJourneyRepository cityJourneyRepository;
 
-
-
-    
     @GetMapping("/cityJourneyList")
-    public ModelAndView cityJourneyList(){
-        ModelAndView model=new ModelAndView("city_journey_list");
+    public ModelAndView cityJourneyList() {
+        ModelAndView model = new ModelAndView("city_journey_list");
 
-        List<CityJourney> departureList=cityJourneyRepository.findByOrderByDepartureAsc();
+        List<CityJourney> departureList = cityJourneyRepository.findByOrderByDepartureAsc();
 
-        model.addObject("cityJourneys",departureList);
+        model.addObject("cityJourneys", departureList);
 
         return model;
 
     }
-
-
 
     @GetMapping("/checkTariff")
-    public ModelAndView departureList(){
-        ModelAndView model=new ModelAndView("check_Tariff");
+    public ModelAndView departureList() {
+        ModelAndView model = new ModelAndView("check_tariff");
 
-        List<CityJourney> departureList=cityJourneyRepository.findByOrderByDepartureAsc();
+        SearchingAttribute searchingAttribute = new SearchingAttribute();
 
-        model.addObject("cityJourneys",departureList);
+        List<CityJourney> departureList = cityJourneyRepository.findByOrderByDepartureAsc();
 
-        return model;
-
-    }
-
-
-
-
-    @GetMapping("/checkTariff")
-    public ModelAndView checkTariff(){
-        ModelAndView model=new ModelAndView("check_Tariff");
-
-        SearchingAttribute searchingAttribute= new SearchingAttribute() ;
-
-        model.addObject("searchingAttribute",searchingAttribute);
+        model.addObject("cityJourneys", departureList);
+        model.addObject("searchingAttribute", searchingAttribute);
 
         return model;
 
     }
 
+    // @GetMapping("/checkTariff")
+    // public ModelAndView checkTariff(){
+    // ModelAndView model=new ModelAndView("check_Tariff");
 
+    // // SearchingAttribute searchingAttribute= new SearchingAttribute() ;
 
-    @PostMapping(path = "/searchTariff")
-    public ModelAndView showTariff(@ModelAttribute SearchingAttribute searchingAttribute){
+    // // model.addObject("searchingAttribute",searchingAttribute);
 
-        ModelAndView model=new ModelAndView("check_Tariff");
+    // return model;
 
-        CityJourney cityJourney=cityJourneyRepository.searchTariff(searchingAttribute.getDeparture(),
-         searchingAttribute.getDestination());
+    // }
 
-         if (cityJourney != null){
-             model.addObject("departure",searchingAttribute.getDeparture());
+    @PostMapping("/searchTarrif")
+    public ModelAndView showTariff(@ModelAttribute("searchingAttribute") SearchingAttribute searchingAttribute) {
 
-             model.addObject("destination", searchingAttribute.getDestination());
+        ModelAndView model = new ModelAndView("check_Tariff");
 
-             model.addObject("tariff",cityJourney.getTariff() );
+        CityJourney cityJourney = cityJourneyRepository.search(searchingAttribute.getDeparture(),
+                searchingAttribute.getDestination());
+        log.error("{}", cityJourney);
+        if (cityJourney != null) {
+            model.addObject("departure", cityJourney.getDeparture());
 
-         }
-         else{
-            model.addObject("departure",searchingAttribute.getDeparture());
+            model.addObject("destination", cityJourney.getDestination());
+
+            model.addObject("tariff", cityJourney.getTariff());
+
+        } else {
+            model.addObject("departure", searchingAttribute.getDeparture());
 
             model.addObject("destination", searchingAttribute.getDestination());
 
-            model.addObject("tariff","No Match Found" );
+            model.addObject("tariff", "No Match Found");
 
-         }
-         return model ;
-
-
-    }
-
-
-
-
-
-    @GetMapping("/addCityJourney")
-    public ModelAndView addCityJourney(){
-
-        ModelAndView model=new ModelAndView("add_city_journey_form");
-
-        CityJourney cityJourney=new CityJourney();
-
-        model.addObject("newCityJourney", cityJourney);
-
+        }
         return model;
 
     }
 
-
-
-
-    @PostMapping("/saveCityJOurney")
-    public String saveCityJourney(@ModelAttribute CityJourney cityJourney){
-
-        cityJourneyRepository.save(cityJourney);
-
-        return "redirect:/cityJourneyList";
+    @GetMapping("/index")
+    public ModelAndView home() {
+        ModelAndView model = new ModelAndView("index");
+        return model;
 
     }
 
+    @GetMapping("/addCityJourney")
+    public ModelAndView addCityJourney() {
+        ModelAndView model = new ModelAndView("add_city_journey_form");
+        CityJourney cityJourney = new CityJourney();
+        model.addObject("newCityJourney", cityJourney);
+        return model;
+    }
 
-
+    @PostMapping("/saveCityJourney")
+    public String saveCityJourney(@ModelAttribute("newCityJourney") CityJourney cityJourney) {
+        
+        cityJourneyRepository.save(cityJourney);
+        return "redirect:/addCityJourney";
+    }
 
     @GetMapping("/deleteCityJourney")
-    public String deleteCityJourney(@RequestParam Long cityJourneyId){
+    public String deleteCityJourney(@RequestParam Long cityJourneyId) {
         cityJourneyRepository.deleteById(cityJourneyId);
 
         return "redirect:/cityJourneyList";
 
     }
 
-
-
-
     @GetMapping("/updateCityJourney")
-    public ModelAndView updateCityJourney(@RequestParam Long cityJourneyId){
+    public ModelAndView updateCityJourney(@RequestParam Long cityJourneyId) {
 
-        ModelAndView model=new ModelAndView("add_city_journey_form");
+        ModelAndView model = new ModelAndView("add_city_journey_form");
 
-        CityJourney cityJourney=cityJourneyRepository.findById(cityJourneyId).get();
+        CityJourney cityJourney = cityJourneyRepository.findById(cityJourneyId).get();
 
         model.addObject("cityJourney", cityJourney);
 
@@ -149,6 +133,4 @@ public class CityJourneyController {
 
     }
 
-
-    
 }
--- imala/src/main/java/com/imala/imala/CityJourneyRepository.java
+++ imala/src/main/java/com/imala/imala/CityJourneyRepository.java
@@ -6,11 +6,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CityJourneyRepository extends JpaRepository<CityJourney,Long> {
     List<CityJourney> findByOrderByDepartureAsc();
     
-    @Query(value = "select * from table Imala I where I.departure = :departure and I.destination = :destination ")
-    CityJourney searchTariff(@Param("departure") String departure,@Param("destination") String destination);
+    @Query(value = "select * from city_journey I where I.departure = :departure and I.destination = :destination ",nativeQuery =true)
+    CityJourney search(@Param("departure") String departure,@Param("destination") String destination);
     
 }
