384d23624520e6eb7b1232038dc743f5c75ecbbd
Abel-Mekonen <abelmekonen559@gmail.com>
2022-01-16 23:28:42 +0300

    city journey updated

--- imala/src/main/java/com/imala/imala/CityJourneyRepository.java
+++ imala/src/main/java/com/imala/imala/CityJourneyRepository.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CityJourneyRepository extends JpaRepository<CityJourney,Long> {
     List<CityJourney> findByOrderByDepartureAsc();
-    List<CityJourney> findByOrderByDestinationAsc();
+    
+    @Query(value = "select * from table Imala I where I.departure = :departure and I.destination = :destination ")
+    CityJourney searchTariff(@Param("departure") String departure,@Param("destination") String destination);
     
 }
