a3affd11d1ea0cac0118e1f60517da6fdc0e27b1
libanabduba <libenpmat@gmail.com>
2022-01-26 22:42:34 +0300

    edited

--- imala/src/main/java/com/imala/imala/security/SecurityConfig.java
+++ imala/src/main/java/com/imala/imala/security/SecurityConfig.java
@@ -1,5 +1,4 @@
 package com.imala.imala.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
--- imala/src/main/java/com/imala/imala/security/WebConfig.java
+++ imala/src/main/java/com/imala/imala/security/WebConfig.java
@@ -1,12 +1,11 @@
 package com.imala.imala.security;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class Webconfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void  addViewControllers(ViewControllerRegistry viewControllerRegistry){
